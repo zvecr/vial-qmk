a3f7f8a43d9933d7e4fc285f680c967d8e7743ed
zvecr <git@zvecr.com>
2023-09-17 06:48:05 +0100

    Merge upstream fixes

--- keyboards/drop/lib/common.c
+++ keyboards/drop/lib/common.c
@@ -1,15 +1,13 @@
 // Copyright 2023 Massdrop, Inc.
 // SPDX-License-Identifier: GPL-2.0-or-later
+
 #ifdef RGB_MATRIX_ENABLE
 #    include "host.h"
-#    include "quantum.h"
 #    include "rgb_matrix.h"
 
 #    ifdef RGB_MATRIX_CAPS_LOCK_INDEX
 bool rgb_matrix_indicators_kb(void) {
-    if (!rgb_matrix_indicators_user()) {
-        return false;
-    }
+    if (!rgb_matrix_indicators_user()) { return false; }
 
     if (host_keyboard_led_state().caps_lock && (rgb_matrix_get_flags() != LED_FLAG_NONE)) {
         rgb_matrix_set_color(RGB_MATRIX_CAPS_LOCK_INDEX, RGB_WHITE);
@@ -18,13 +16,10 @@ bool rgb_matrix_indicators_kb(void) {
     }
     return true;
 }
-#    endif
+#endif
 
-#    ifdef RGB_MATRIX_CYCLE_ZONES_ENABLE
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
-    if (!process_record_user(keycode, record)) {
-        return false;
-    }
+    if (!process_record_user(keycode, record)) { return false; }
 
     if (!record->event.pressed) {
         switch (keycode) {
@@ -48,6 +43,5 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 };
-#    endif
 
 #endif
