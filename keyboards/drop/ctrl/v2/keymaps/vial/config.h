e546e9e5d554e974911fa5b33fad5587b1c4f353
zvecr <git@zvecr.com>
2023-09-17 06:40:35 +0100

    Fix up vial keymaps

--- keyboards/drop/ctrl/v2/keymaps/vial/config.h
+++ keyboards/drop/ctrl/v2/keymaps/vial/config.h
@@ -1,5 +1,10 @@
-// Copyright 2023 Massdrop, Inc.
+// Copyright 2022 Massdrop, Inc.
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
+
+#define VIAL_KEYBOARD_UID {0x44, 0xA8, 0xDC, 0xFF, 0x05, 0x4E, 0x05, 0x53}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 9 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
--- keyboards/drop/shift/v2/keymaps/vial/config.h
+++ keyboards/drop/shift/v2/keymaps/vial/config.h
@@ -1,5 +1,10 @@
-// Copyright 2023 Massdrop, Inc.
+// Copyright 2022 Massdrop, Inc.
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
+
+#define VIAL_KEYBOARD_UID {0x2A, 0x3F, 0x00, 0xE5, 0x78, 0xF6, 0xE8, 0xEA}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 6 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 8 }
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
