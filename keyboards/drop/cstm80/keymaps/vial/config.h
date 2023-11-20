51e57f316953922af725f9471f6370f360c82f43
zvecr <git@zvecr.com>
2023-11-20 13:32:33 +0000

    Add cstm

---
+++ keyboards/drop/cstm80/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+// Copyright 2023 Massdrop, Inc.
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE4, 0x07, 0x81, 0x57, 0xF8, 0x58, 0x72, 0x57}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
